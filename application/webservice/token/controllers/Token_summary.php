bf4b8409c02f296fc7784a7b78e08b800ca118a7
Aarbaj <aarbaj.mulla@hiddenbrains.in>
2026-01-18 02:01:16 +0530

    Response change for summry token

--- application/webservice/token/controllers/Token_summary.php
+++ application/webservice/token/controllers/Token_summary.php
@@ -19,12 +19,14 @@ class Token_summary extends My_Api_Controller
         $start_date = date('Y-m-d');
         $end_date = date('Y-m-d');
         $items_data = $this->token_summary_model->get_month_tokens($restaurant_id,$start_date,$end_date);
-        $items_data = array_column($items_data, null, 'payment_mode');
+        $total_price = array_sum(array_column($items_data, 'total_price'));
+        $total_tokens = array_sum(array_column($items_data, 'total_tokens'));
+        $items_data_arr = array_column($items_data, null, 'payment_mode');
         $summary_data['today'] = [
-            "total_revenue_online" => isset($items_data['Online']['total_price']) && $items_data['Online']['total_price'] > 0 ? number_format($items_data['Online']['total_price'],2) : 0,
-            "total_tokens_online" => isset($items_data['Online']['total_tokens']) && $items_data['Online']['total_tokens'] > 0 ? number_format($items_data['Online']['total_tokens']) : 0,
-            "total_revenue_cash" => isset($items_data['Cash']['total_price']) && $items_data['Cash']['total_price'] > 0 ? number_format($items_data['Cash']['total_price'],2) : 0,
-            "total_tokens_cash" => isset($items_data['Cash']['total_tokens']) && $items_data['Cash']['total_tokens'] > 0 ? number_format($items_data['Cash']['total_tokens']) : 0
+            "total_revenue" => $total_price > 0 ? number_format($total_price,2) : 0,
+            "total_tokens" => $total_tokens > 0 ? number_format($total_tokens) : 0,
+            "total_revenue_online" => isset($items_data_arr['Online']['total_price']) && $items_data_arr['Online']['total_price'] > 0 ? number_format($items_data_arr['Online']['total_price'],2) : 0,
+            "total_revenue_cash" => isset($items_data_arr['Cash']['total_price']) && $items_data_arr['Cash']['total_price'] > 0 ? number_format($items_data_arr['Cash']['total_price'],2) : 0
 
         ];
 
@@ -32,12 +34,14 @@ class Token_summary extends My_Api_Controller
         $start_date = date('Y-m-01');
         $end_date = date('Y-m-d');
         $items_data = $this->token_summary_model->get_month_tokens($restaurant_id,$start_date,$end_date);
-        $items_data = array_column($items_data, null, 'payment_mode');
+        $total_price = array_sum(array_column($items_data, 'total_price'));
+        $total_tokens = array_sum(array_column($items_data, 'total_tokens'));
+        $items_data_arr = array_column($items_data, null, 'payment_mode');
         $summary_data['current_month'] = [
-            "total_revenue_online" => isset($items_data['Online']['total_price']) && $items_data['Online']['total_price'] > 0 ? number_format($items_data['Online']['total_price'],2) : 0,
-            "total_tokens_online" => isset($items_data['Online']['total_tokens']) && $items_data['Online']['total_tokens'] > 0 ? number_format($items_data['Online']['total_tokens']) : 0,
-            "total_revenue_cash" => isset($items_data['Cash']['total_price']) && $items_data['Cash']['total_price'] > 0 ? number_format($items_data['Cash']['total_price'],2) : 0,
-            "total_tokens_cash" => isset($items_data['Cash']['total_tokens']) && $items_data['Cash']['total_tokens'] > 0 ? number_format($items_data['Cash']['total_tokens']) : 0
+            "total_revenue" => $total_price > 0 ? number_format($total_price,2) : 0,
+            "total_tokens" => $total_tokens > 0 ? number_format($total_tokens) : 0,
+            "total_revenue_online" => isset($items_data_arr['Online']['total_price']) && $items_data_arr['Online']['total_price'] > 0 ? number_format($items_data_arr['Online']['total_price'],2) : 0,
+            "total_revenue_cash" => isset($items_data_arr['Cash']['total_price']) && $items_data_arr['Cash']['total_price'] > 0 ? number_format($items_data_arr['Cash']['total_price'],2) : 0
 
         ];
 
@@ -45,24 +49,28 @@ class Token_summary extends My_Api_Controller
         $start_date = date('Y-m-01', strtotime('first day of last month'));
         $end_date   = date('Y-m-t', strtotime('last month'));
         $items_data = $this->token_summary_model->get_month_tokens($restaurant_id,$start_date,$end_date);
-        $items_data = array_column($items_data, null, 'payment_mode');
+        $total_price = array_sum(array_column($items_data, 'total_price'));
+        $total_tokens = array_sum(array_column($items_data, 'total_tokens'));
+        $items_data_arr = array_column($items_data, null, 'payment_mode');
         $summary_data['last_month'] = [
-            "total_revenue_online" => isset($items_data['Online']['total_price']) && $items_data['Online']['total_price'] > 0 ? number_format($items_data['Online']['total_price'],2) : 0,
-            "total_tokens_online" => isset($items_data['Online']['total_tokens']) && $items_data['Online']['total_tokens'] > 0 ? number_format($items_data['Online']['total_tokens']) : 0,
-            "total_revenue_cash" => isset($items_data['Cash']['total_price']) && $items_data['Cash']['total_price'] > 0 ? number_format($items_data['Cash']['total_price'],2) : 0,
-            "total_tokens_cash" => isset($items_data['Cash']['total_tokens']) && $items_data['Cash']['total_tokens'] > 0 ? number_format($items_data['Cash']['total_tokens']) : 0
+            "total_revenue" => $total_price > 0 ? number_format($total_price,2) : 0,
+            "total_tokens" => $total_tokens > 0 ? number_format($total_tokens) : 0,
+            "total_revenue_online" => isset($items_data_arr['Online']['total_price']) && $items_data_arr['Online']['total_price'] > 0 ? number_format($items_data_arr['Online']['total_price'],2) : 0,
+            "total_revenue_cash" => isset($items_data_arr['Cash']['total_price']) && $items_data_arr['Cash']['total_price'] > 0 ? number_format($items_data_arr['Cash']['total_price'],2) : 0
 
         ];
 
         /* overall */
         
         $items_data = $this->token_summary_model->get_month_tokens($restaurant_id);
-        $items_data = array_column($items_data, null, 'payment_mode');
+        $total_price = array_sum(array_column($items_data, 'total_price'));
+        $total_tokens = array_sum(array_column($items_data, 'total_tokens'));
+        $items_data_arr = array_column($items_data, null, 'payment_mode');
         $summary_data['overall'] = [
-            "total_revenue_online" => isset($items_data['Online']['total_price']) && $items_data['Online']['total_price'] > 0 ? number_format($items_data['Online']['total_price'],2) : 0,
-            "total_tokens_online" => isset($items_data['Online']['total_tokens']) && $items_data['Online']['total_tokens'] > 0 ? number_format($items_data['Online']['total_tokens']) : 0,
-            "total_revenue_cash" => isset($items_data['Cash']['total_price']) && $items_data['Cash']['total_price'] > 0 ? number_format($items_data['Cash']['total_price'],2) : 0,
-            "total_tokens_cash" => isset($items_data['Cash']['total_tokens']) && $items_data['Cash']['total_tokens'] > 0 ? number_format($items_data['Cash']['total_tokens']) : 0
+            "total_revenue" => $total_price > 0 ? number_format($total_price,2) : 0,
+            "total_tokens" => $total_tokens > 0 ? number_format($total_tokens) : 0,
+            "total_revenue_online" => isset($items_data_arr['Online']['total_price']) && $items_data_arr['Online']['total_price'] > 0 ? number_format($items_data_arr['Online']['total_price'],2) : 0,
+            "total_revenue_cash" => isset($items_data_arr['Cash']['total_price']) && $items_data_arr['Cash']['total_price'] > 0 ? number_format($items_data_arr['Cash']['total_price'],2) : 0
 
         ];
        
