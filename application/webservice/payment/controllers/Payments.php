f1e362a61997b47a11ea5b79709c312552ed13f1
Aarbaj <aarbaj.mulla@hiddenbrains.in>
2026-01-24 16:41:54 +0530

    Payment order issue1

--- application/webservice/payment/controllers/Payments.php
+++ application/webservice/payment/controllers/Payments.php
@@ -61,7 +61,7 @@ class Payments extends My_Api_Controller
             "status" => $response['status'],
             "response_json" => json_encode($data)
         ];
-        $get_transaction = $this->payment_model->get_transaction($response['id']);
+        $get_transaction = $this->payment_model->get_transaction($response['id']); 
         $this->payment_success($get_transaction);
         if(!(count($get_transaction) > 0) || $get_transaction == null){
             $payment_entry = $this->payment_model->create($insert_data);
